0444f1c83af149117bbf02c034b52f7ba15f2206
caignaert <davidcaignaert@gmail.com>
2020-11-16 15:38:13 +0100

    finalisation de search .php avec le terme rechercher

--- search.php
+++ search.php
@@ -2,7 +2,7 @@
 get_header();
 ?>
 <div class="category">
-<h1>terme de recherche : xxxxxxxxxx<?php single_cat_title(); ?></h1>
+<h1>terme de recherche <?php the_search_query(); ?></h1>
 <?php get_template_part('loop'); ?>
 </div>
 <?php
